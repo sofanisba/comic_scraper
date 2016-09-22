6071c5524da6baf8142a8f55229d5bfa45ceb25c
sofanisba <stefanov.tamara@gmail.com>
2016-09-22 19:21:00 -0400

    objects for each comic

--- scraper.rb
+++ scraper.rb
@@ -5,18 +5,24 @@ require 'pry'
 
 url = 'http://www.comics.org/on_sale_weekly/'
 page = Nokogiri::HTML(open(url))
-this_week = {}
+blk = lambda {|k,v| k[v] = Hash.new(&blk)}
+this_week = Hash.new(&blk)
 
 rows = page.css('tr')
 
-rows[1..-2].each do |r|
-  title = rows.css('td a').map {|a|
-    a.text if a['href'].match('/series/')
-  }.compact.uniq
+comics = rows.collect do |row|
+  comic = {}
+  publisher = row.css('td a').map {|a| a.text if a['href'].match('/publisher')}.compact.uniq
+  title = row.css('td a').map {|a| a.text if a['href'].match('/series')}.compact.uniq
+  issue = row.css('td a').map {|a| a.text if a['href'].match('/issue')}.compact.uniq
 
-  issue_num = rows.css('td a').map {|a|
-    a.text if a['href'].match('/issue/')
-  }.compact.uniq
-
-  this_week = JSON.dump(title: title, issue_num: issue_num)
+  [
+    [:publisher, publisher],
+    [:title, title],
+    [:issue, issue]
+  ].each do |k,v|
+    comic[k] = v
+  end
+  comic
 end
+pp comics
