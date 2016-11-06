7750029942a88cdd7920b734d162f613d196f1d5
sofanisba <stefanov.tamara@gmail.com>
2016-11-05 20:22:38 -0400

    link click + pub date

--- scraper.rb
+++ scraper.rb
@@ -4,32 +4,42 @@ require 'mechanize'
 require 'json'
 require 'pry'
 
+
 agent = Mechanize.new
+
 url = 'http://www.comics.org/on_sale_weekly/'
 page = agent.get(url)
-# puts page.uri
+
+module Enumerable
+  def every_nth(n)
+    (n - 1).step(self.size - 1, n).map { |i| self[i] }
+  end
+end
+
 blk = lambda {|k,v| k[v] = Hash.new(&blk)}
 this_week = Hash.new(&blk)
 
-rows = page.css('tr')
 next_link = page.link_with(text: 'Next >')
 page= next_link.click
+rows = page.css('tr')
 
 comics = rows.collect do |row|
   comic = Hash.new
   publisher = row.css('td a').map {|a| a.text if a['href'].match('/publisher')}.compact.uniq.join(" , ")
   title = row.css('td a').map {|a| a.text if a['href'].match('/series')}.compact.uniq.join(" , ")
   issue = row.css('td a').map {|a| a.text if a['href'].match('/issue')}.compact.uniq.join(" , ")
+
+  pub_date = row.css('td').every_nth(4).map {|td| td.text}
   [
     [:publisher, publisher],
     [:title, title],
-    [:issue, issue]
+    [:issue, issue],
+    [:pub_date, pub_date]
   ].each do |k,v|
     comic[k] = v
   end
   comic
 end
 
-# puts page.uri
 # binding.pry
 puts comics
